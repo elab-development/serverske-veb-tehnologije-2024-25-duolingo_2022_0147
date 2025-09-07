a72e31222cb0be66ed4812ab03b6d1ec424d80d9
BogdanMrdeljic <bogdanmrdeljic10032003@gmail.com>
2025-09-07 17:03:52 +0200

    Api route updated

--- duolingo/routes/api.php
+++ duolingo/routes/api.php
@@ -3,6 +3,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\EnrollmentController;
 use App\Http\Controllers\LessonController;
+use App\Http\Controllers\TranslateController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,6 +21,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
 Route::get('/courses', [CourseController::class, 'index']);
 Route::get('/courses/{course}', [CourseController::class, 'show']);
 
@@ -30,10 +32,19 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+
     Route::resource('courses', CourseController::class)
         ->only(['store', 'update', 'destroy']);
+
+    Route::get('/teacher/{id}/courses', [CourseController::class, 'teacherCourses']);
+
     Route::resource('/lessons', LessonController::class);
+
     Route::resource('/enrollments', EnrollmentController::class)
         ->only(['index', 'store', 'update']);
+
+    Route::get('/student/{id}/enrollments', [EnrollmentController::class, 'studentEnrollments']);
+
+    Route::get('/translate', [TranslateController::class, 'translate']);
    
 });
